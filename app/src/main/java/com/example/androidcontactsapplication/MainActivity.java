f99e53f9078abdaab2123d57d8f5e498435f4672
kushal <kushal_vaghani@yahoo.com>
2022-03-29 20:28:07 -0400

    UI

--- app/src/main/java/com/example/androidcontactsapplication/MainActivity.java
+++ app/src/main/java/com/example/androidcontactsapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.androidcontactsapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /*_________________________________________
+
+                Creating the Menu Item
+    ____________________________________________*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+      /*_________________________________________
+
+                Adding the Menu Item
+      ____________________________________________*/
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add:
+                Intent intentAdd = new Intent(this, AddContact.class);
+                startActivity(intentAdd);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
